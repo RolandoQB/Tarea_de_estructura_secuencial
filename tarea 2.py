3aab01d787e063cbb86ff29c16d9f5b574d5126d
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-23 23:21:56 -0500

    Update tarea 2.py

--- tarea 2.py
+++ tarea 2.py
@@ -4,6 +4,11 @@
 
 #Tarea 2 de ejercicios de pagina web
 
+"""EJEMPLO 2:
+En una tienda se ofrece un descuento del 15% sobre el 
+total de la compra y un cliente desea saber cuánto deberá 
+pagar finalmente por su compra."""
+
 def run():
     print("_______________________________________")
     TC=float(input("Ingrese total de la compra:"))
