0f50d02636ddffee45ea52d13096e4ffa9dae650
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-23 23:36:38 -0500

    Create tarea5.py

---
+++ tarea5.py
@@ -0,0 +1,28 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+#Tarea 5 de ejercicios de pagina web
+
+"""EJEMPLO 5:
+ Dado como dato la calificación de un alumno en un examen, 
+ escriba “aprobado” si su calificación es mayor o igual que 7 
+ y “Reprobado” en caso contrario."""
+
+def run():
+    print("_______________________________________")
+    cal=float(input("Ingrese su calificacion:"))
+    if cal >= 7 :
+        print()
+        print("!!!!!!Felicidades!!!!!!")
+        print("       APROBADO")
+    else:
+        print()
+        print("!!!!!!Ho no, esfuerzate mas!!!!!!")
+        print("          REPROBADO")
+    print()
+    print("_______________________________________")
+    input("enter para salir")
+
+if __name__ == "__main__":
+    run()
