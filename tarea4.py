ebd70d90d1cf54b7f7d36bd81287acdf9bc632e2
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-23 23:30:50 -0500

    Create tarea4.py

---
+++ tarea4.py
@@ -0,0 +1,28 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+#Tarea 4 de ejercicios de pagina web
+
+"""EJEMPLO 4
+   Construir un algoritmo tal, que dado como dato la 
+   calificación de un alumno en un examen, escriba"""
+
+def run():
+    print("_______________________________________")
+    cal=float(input("Ingrese su calificacion:"))
+    if cal >= 7 :
+        print()
+        print("!!!!!!Felicidades!!!!!!")
+        print("       APROBADO")
+    else:
+        print()
+        print("!!!!!!Ho no, esfuerzate mas!!!!!!")
+        print("          REPROBADO")
+    print()
+    print("_______________________________________")
+    input("enter para salir")
+
+
+if __name__ == "__main__":
+    run()
