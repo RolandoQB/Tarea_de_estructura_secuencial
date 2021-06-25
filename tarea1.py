0691466f8899b41071f26b625937329df97f9693
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-25 09:25:11 -0500

    Update tarea1.py

--- tarea1.py
+++ tarea1.py
@@ -9,17 +9,20 @@ En ejemplos anteriores, diseñamos un pseudocódigo para encontrar la
 superficie de un círculo para un radio cualquiera.
 El Flujograma que representa a dicho ejemplo es el siguiente:"""
 
-def run():
-    pi=3.1416
-    R =int(input("Ingrese el Radio del circulo : "))
-    print("_______________________________________")
-    cal =R*R
-    S=cal*pi
-    print("La superficie del circulo es: ")
-    print(S)
-    print("_______________________________________")
+class Tarea1:
+    def __init__(self):
+        pass
+    def Calcular(self):
+        pi=3.1416
+        R =int(input("Ingrese el Radio del circulo : "))
+        print("_______________________________________")
+        cal =R*R
+        S=cal*pi
+        print("La superficie del circulo es: ")
+        print(S)
+        print("_______________________________________")
 
-    input("enter para salir")
+        input("enter para salir")
 
-if __name__ == "__main__":
-    run()
+tarea= Tarea1()
+tarea.Calcular()
