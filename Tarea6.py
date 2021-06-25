a9a5dc990e44ab368a54a1cf4aa079223078f13c
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-25 09:15:08 -0500

    Update Tarea6.py

--- Tarea6.py
+++ Tarea6.py
@@ -9,19 +9,22 @@
 Dado el sueldo de un empleado, encontrar el nuevo sueldo si obtiene un aumento 
 del 10% si su sueldo es inferior a $600, en caso contrario no tendrá aumento."""
 
-def run():
-    print("_______________________________________")
-    SUELDO=float(input("Sueldo de empleados:"))
-    if SUELDO < 600:
-        NS=SUELDO + SUELDO*0.1
-        print()
-    else:
-        NS=SUELDO
-        print()
-    print("_______________________________________")    
-    print(NS)
-    print("_______________________________________")
-    input("enter para salir")
+class Tarea:
+    def __init__(self):
+        pass
+    def Calcular(self):
+        print("_______________________________________")
+        SUELDO=float(input("Sueldo de empleados:"))
+        if SUELDO < 600:
+            NS=SUELDO + SUELDO*0.1
+            print()
+        else:
+            NS=SUELDO
+            print()
+        print("_______________________________________")    
+        print(NS)
+        print("_______________________________________")
+        input("enter para salir")
 
-if __name__ == "__main__":
-    run()
+tarea=Tarea()
+tarea.Calcular()
