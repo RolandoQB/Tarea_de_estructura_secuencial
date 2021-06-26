fc282af12df621cb4d821c198f44ec468d30cc6e
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-26 18:43:24 -0500

    Create tarea13.py

---
+++ tarea13.py
@@ -0,0 +1,27 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+#Tarea 13 de ejercicios de pagina web
+
+"""Ejercicio 13
+
+Calcular la suma de los cuadrados de los primeros 
+100 enteros y escribir el resultado."""
+
+class Tarea13:
+    def __init__ (self):
+        pass
+    def Variables(self):
+        print("_______________________________________")
+        print("numeros hasta el 100 con While")
+        i=1
+        while i <=100:
+            print(i)
+            i=i+1
+                       
+        print("_______________________________________")
+        input("enter para salir") 
+        
+resultado = Tarea13()
+resultado.Variables()
