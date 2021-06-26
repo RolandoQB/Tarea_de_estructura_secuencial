99f9d17d2c5c1b270048d4cf60debc96176d8625
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-26 15:07:17 -0500

    Update Tarea8.py

--- Tarea8.py
+++ Tarea8.py
@@ -10,13 +10,13 @@ Leer tres números enteros diferentes entre sí y
 determinar el número mayor de los tres."""
 
 class Tarea8:
-    def __init__(self):
+    def __init__ (self):
         pass
     def NumeroMayor(self):
         print("_______________________________________")
-        n1= int(input("Ingrese primer número entero: "))
-        n2= int(input("Ingrese segundo número entero: "))
-        n3= int(input("Ingrese tercer número entero: "))
+        n1= int(input("Ingrese primer numero entero: "))
+        n2= int(input("Ingrese segundo numero entero: "))
+        n3= int(input("Ingrese tercer numero entero: "))
         print("_______________________________________")
         if n1>n2 and n1>n3:
             nM=n1
@@ -25,9 +25,8 @@ class Tarea8:
                 nM=n2
             else:
                 nM=n3
-        print("El numero Mayor es: {}".format(nM))
+        print("El numero Mayor es:",nM)
         print("_______________________________________")
         input("enter para salir") 
-
-tarea = Tarea8()
-tarea.NumeroMayor()
+objeto = Tarea8()
+objeto.NumeroMayor()
